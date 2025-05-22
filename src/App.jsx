39bcf625d4333f49976a3670a6b8d1422630b368
jakkamakhil7036 <akhiljakkam12@gmail.com>
2025-05-22 18:31:25 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { Route } from 'react-router-dom'
-import Product from "./components/Product"
-import Cart from "./components/Cart"
+// import { Route } from 'react-router-dom'
+import Product from './components/Product'
+import Cart from './components/Cart'
 import { BrowserRouter,Routes,Route,Link } from 'react-router-dom'
+
 
 function App() {
  
@@ -16,14 +17,14 @@ function App() {
       <header>
      <h1>My Online Shop</h1>
      <Link to="/">Home</Link>
-     <Link to="/cart">cart</Link>
+     <Link to="/cart"> cart</Link>
      <hr/>
      </header>
      <main>
      <Routes>
     <Route index element={<Product />}/>
     <Route path="/" element={<Product/>}></Route>
-    <Route path="/cart" element={<cart/>}></Route>
+    <Route path="/cart" element={<Cart/>}></Route>
         </Routes>
       </main>
       <footer>
